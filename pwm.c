858b18b51ff3881f84bc428a1052d385629c0d0a
elifsezin <65909888+elifsezin@users.noreply.github.com>
2024-05-17 19:45:35 +0300

    Update pwm.c

--- pwm.c
+++ pwm.c
@@ -10,29 +10,28 @@
 
 /*
  * Assuming maximum distance of interest is 5m:
- * Maximum travel time of the sound wave is 3 ms.
- * Therefore the "listen" time must be at least 3 ms.
+ * Maximum travel time of the sound wave is 30 ms.
+ * Therefore the "listen" time must be at least 30 ms.
  *
  * In each burst, it is required to have 20 pulses. 
  * At nominal frequency og 40 kHz, the burst duration is 500 us.
  * Changes in burst duration w.r.t. frequency are neglected.
 
- * Therefore the total period of one transmit cycle must be 3.5 ms.
- * Which results in 285 Hz frequency for the inhibit pin control PWM.
+ * Therefore the total period of one transmit cycle must be 30.5 ms.
+ * Which results in 32.79 Hz frequency for the inhibit pin control PWM.
 
-
-
- * For the nominal 40 kHz, the duty therefore must be 6/7.
- * Out of 1M, that equals to 857e3.
+ * For the nominal 40 kHz, the duty therefore must be 60/61.
+ * duty = 1 - (500e-6)/(30*5e-3) = 60/61
+ * Out of 1M, that equals to 983607.
 */
 
-#define PWM_FREQ 285
-#define PWM_DUTY 857e3
+#define PWM_FREQ 32.79
+#define PWM_DUTY 983607
 
 
 int calculate_duty( const int freq )
 {
-    return ( 1.0 - ( ( 20.0 / freq ) / 3.5e-3 ) ) * 1e6 ;
+    return ( 1.0 - ( ( 20.0 / freq ) / 30.5e-3 ) ) * 1e6 ;
 }
 
 
